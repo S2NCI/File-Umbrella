2552d1ef9bdc96913abebdd3c5e9a01c8ff8e821
Adam Malone <adam-malone@hotmail.co.uk>
2023-04-03 21:41:46 +0100

    can now add folder id, name, password, user IP to database. Login will check validity

--- File_Umbrella/src/main/java/Controllers/CreateFolderController.java
+++ File_Umbrella/src/main/java/Controllers/CreateFolderController.java
@@ -14,18 +14,25 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import org.bson.Document;
 
 import java.io.IOException;
+import java.util.UUID;
 
-public class AccountController {
+public class CreateFolderController {
     @FXML
     public Label btnCreateFolder;
     @FXML
-    Label userIPLabel;
+    public TextField folderGen;
+    @FXML
+    public TextField folderPass;
+    @FXML
+    public TextField folderNameTF;
 
 
     @FXML
@@ -76,7 +83,52 @@ public class AccountController {
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
+    }
 
+    @FXML
+    private void cancelCreate(MouseEvent event){
+        System.exit(0);
+    }
+
+
+    public String folderIDGeneration(MouseEvent event) {
+        String uuid = UUID.randomUUID().toString().replace("-", "");
+        String generated = uuid.substring(0, 16);
+        folderGen.setText(generated);
+        return uuid;
+    }
+
+
+    public void copyFolderID(MouseEvent event) {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent content = new ClipboardContent();
+        content.putString(folderGen.getText());
+        clipboard.setContent(content);
+    }
+
+    @FXML
+    private void handleFolderCreation(ActionEvent event) throws IOException {
+        String folderID = folderGen.getText();
+        String folderPassword = folderPass.getText();
+        String folderName = folderNameTF.getText();
+        // get user ip
+        String currentIP = null;
+        try {
+            currentIP = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        String connectionString = "mongodb+srv://admin:dbpass@cluster0.jmttrjk.mongodb.net/?retryWrites=true&w=majority";
+        MongoClient mongoClient = DBController.createConnection(connectionString);
+        MongoDatabase database = mongoClient.getDatabase("UserConnection");
+        MongoCollection<Document> collection = database.getCollection("IPAddress");
+
+        Document folderDoc = new Document ("folderId", folderID)
+                .append("folderPassword", folderPassword)
+                .append("folderName", folderName)
+                .append("IPAddress", currentIP);
+
+        collection.insertOne(folderDoc);
     }
 
 }
--- File_Umbrella/src/main/java/Controllers/DBController.java
+++ File_Umbrella/src/main/java/Controllers/DBController.java
@@ -6,7 +6,6 @@ import com.mongodb.client.MongoClients;
 public class DBController {
     // create a method to store the mongodb connection string
     public static MongoClient createConnection(String connectionString) {
-        MongoClient mongoClient = MongoClients.create(connectionString);
-        return mongoClient;
+        return MongoClients.create(connectionString);
     }
 }
